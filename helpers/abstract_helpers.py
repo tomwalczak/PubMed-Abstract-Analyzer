3394e8fdf6ea8303147044e01fd604f67d7684c3
tomwalchak <tom@tomwalczak.com>
2021-06-09 11:42:55 +0100

    added helper functions

--- helpers/abstract_helpers.py
+++ helpers/abstract_helpers.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import nltk
+import tensorflow as tf
 
 
 
@@ -126,3 +127,225 @@ def get_lines(filename):
   with open(filename, "r") as f:
     return f.readlines()
 
+
+# Remix of Scikit-Learn's@mrdbourke's implementation
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+# Our function needs a different name to sklearn's plot_confusion_matrix
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+  """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+
+  If classes is passed, confusion matrix will be labelled, if not, integer class values
+  will be used.
+
+  Args:
+    y_true: Array of truth labels (must be same shape as y_pred).
+    y_pred: Array of predicted labels (must be same shape as y_true).
+    classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+    figsize: Size of output figure (default=(10, 10)).
+    text_size: Size of output figure text (default=15).
+    norm: normalize values or not (default=False).
+    savefig: save confusion matrix to file (default=False).
+  
+  Returns:
+    A labelled confusion matrix plot comparing y_true and y_pred.
+
+  Example usage:
+    make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                          y_pred=y_preds, # predicted labels
+                          classes=class_names, # array of class label names
+                          figsize=(15, 15),
+                          text_size=10)
+  """  
+  # Create the confustion matrix
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+  n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+  # Plot the figure and make it pretty
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+  fig.colorbar(cax)
+
+  # Are there a list of classes?
+  if len(classes) > 1:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+  
+  # Label the axes
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes), # create enough axis slots for each class
+         yticks=np.arange(n_classes), 
+         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+         yticklabels=labels)
+  
+  # Make x-axis labels appear on bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  ### Added: Rotate xticks for readability & increase font size (required due to such a large confusion matrix)
+  plt.xticks(rotation=70, fontsize=text_size)
+  plt.yticks(fontsize=text_size)
+
+  # Set the threshold for different colors
+  threshold = (cm.max() + cm.min()) / 2.
+
+  # Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Save the figure to the current working directory
+  if savefig:
+    fig.savefig("confusion_matrix.png")
+
+
+def model_preds_df(y_true_labels_int, y_preds, sentences, class_names):
+  pred_df = pd.DataFrame({
+      "y_true": y_true_labels_int,
+      "y_pred": y_preds,
+      "y_true_class_name": [class_names[pred] for pred in y_true_labels_int],
+      "y_pred_class_name": [class_names[pred] for pred in pred_classes],
+      "pred_confidence": preds_probs.max(axis=1),
+  })
+
+  pred_df["is_pred_correct"] = pred_df["y_true"] == pred_df["y_pred"]
+  pred_df["sentence"] = sentences
+
+  return pred_df
+
+# Credit: @mrdbourke
+def create_tensorboard_callback(dir_name, experiment_name):
+  """
+  Creates a TensorBoard callback instand to store log files.
+
+  Stores log files with the filepath:
+    "dir_name/experiment_name/current_datetime/"
+
+  Args:
+    dir_name: target directory to store TensorBoard log files
+    experiment_name: name of experiment directory (e.g. efficientnet_model_1)
+  """
+  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(
+      log_dir=log_dir
+  )
+  print(f"Saving TensorBoard log files to: {log_dir}")
+  return tensorboard_callback
+
+# Plot the validation and training data separately
+
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+
+  Args:
+    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+  """ 
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
+
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+# Credit: @mrdbourke
+def calculate_results(y_true, y_pred):
+    """
+    Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+    Args:
+        y_true: true labels in the form of a 1D array
+        y_pred: predicted labels in the form of a 1D array
+
+    Returns a dictionary of accuracy, precision, recall, f1-score.
+    """
+    # Calculate model accuracy
+    model_accuracy = accuracy_score(y_true, y_pred) * 100
+    # Calculate model precision, recall and f1 score using "weighted average
+    model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+    model_results = {"accuracy": model_accuracy,
+                    "precision": model_precision,
+                    "recall": model_recall,
+                    "f1": model_f1}
+    return model_results
+
+# Credit: based on @mrdbourke's code
+def preprocess_text_add_line_position_features(filename):
+    """
+    Returns a list of dictionaries of abstract line data.
+
+    Takes in filename, reads it contents and sorts through each line,
+    extracting things like the target label, the text of the sentnece,
+    how many sentences are in the current abstract and what sentence
+    number the target line is.
+    """
+    input_lines = get_lines(filename) # get all lines from filename
+    abstract_lines = "" # create an empty abstract
+    previous_class = "" # extra feautre for context
+    abstract_samples = [] # create an empty list of abstracts
+
+    # Loop through each line in the target file
+    for line in input_lines:
+        if line.startswith("###"): # check to see if the line is an ID line
+            abstract_id = line
+            abstract_lines = "" # reset the abstract string if the line is an ID line
+            previous_class = "" # reset, since this is a new abstract
+
+        elif line.isspace(): # check to see if line is a new line
+          abstract_line_split = abstract_lines.splitlines() # split abstract into separate lines
+
+        # Iterate through each line in a single abstract and count them at the same time
+        for abstract_line_number, abstract_line in enumerate(abstract_line_split):
+            line_data = {} # create an empty dictionary for each line
+            target_text_split = abstract_line.split("\t") # split target label from text 
+            line_data["target"] = target_text_split[0] # get target label
+            line_data["text"] = target_text_split[1].lower() # get target text and lower it
+            line_data["line_number"] = abstract_line_number # what number line does the line appear in the abstract?
+            line_data["total_lines"] = len(abstract_line_split) - 1 # how many total lines are there in the target abstract? (start from 0)
+            line_data["text_with_pos_feature"] = "POSITION_" + (np.around(line_data["line_number"] / line_data["total_lines"], decimals=2)*100).astype("int").astype("str") + " " + line_data["text"]
+            line_data["text_with_pos_and_prev_feature"] = line_data["text_with_pos_feature"];
+            
+            if previous_class:
+                line_data["text_with_pos_and_prev_feature"] = "PREV_" + previous_class + " " + line_data["text_with_pos_and_prev_feature"]
+
+                previous_class = line_data["target"]
+                
+                abstract_samples.append(line_data) # add line data to abstract samples list
+
+        else: # if the above conditions aren't fulfilled, the line contains a labelled sentence
+           abstract_lines += line
+
+    return abstract_samples
