2078916c712943c1dcab5e141b4276b39f84deb7
tomwalchak <tom@tomwalczak.com>
2021-06-13 22:21:13 +0100

    trying distillBert

--- helpers/summary_helpers.py
+++ helpers/summary_helpers.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from transformers import pipeline
+# from transformers import pipeline
 
-# from transformers import  DistilBertModel,DistilBertTokenizer
+from transformers import  DistilBertModel,DistilBertTokenizer
 # from summarizer import Summarizer
 
 import pickle
@@ -16,10 +16,10 @@ from .nlp_functions import detect_sentences, create_tdidf_doc_term_matrix
 # device = torch.device('cpu')
 
 
-# distillBert = DistilBertModel.from_pretrained("distilbert-base-uncased", output_hidden_states=True)
-# distillBertTokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+distillBert = DistilBertModel.from_pretrained("distilbert-base-uncased", output_hidden_states=True)
+distillBertTokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 
-hf_summarizer = pipeline('summarization')
+# hf_summarizer = pipeline('summarization')
 
 with open("./lib/punkt/PY3/english.pickle","rb") as resource:
   sent_detector = pickle.load(resource)
@@ -64,35 +64,36 @@ def get_summary(full_text, model_name="T5_sum"):
 #     return summary
 
 def summarize_hf_pipeline(abstract):
-    abstract = abstract.replace('.', '.<eos>').replace('?', '?<eos>').replace('!', '!<eos>')
+    return "HF Pipeline not available"
+    # abstract = abstract.replace('.', '.<eos>').replace('?', '?<eos>').replace('!', '!<eos>')
 
-    max_words_in_chunk = 510
-    sents = abstract.split('<eos>')
-    chunk_idx = 0
-    chunks = []
-    for sentence in sents:
-        if len(chunks) == chunk_idx + 1:
-            # Check if adding current sentence's words would exceed max words limit
-            if len(chunks[chunk_idx]) + len(sentence.split(' ')) <= max_words_in_chunk:
-                chunks[chunk_idx].extend(sentence.split(' '))
-            else:
-                chunk_idx += 1
-                # Append sentence words into the current chunk
-                chunks.append(sentence.split(' '))
-        else:
-            print(chunk_idx)
-            # Append sentence words into the current chunk
-            chunks.append(sentence.split(' '))
+    # max_words_in_chunk = 510
+    # sents = abstract.split('<eos>')
+    # chunk_idx = 0
+    # chunks = []
+    # for sentence in sents:
+    #     if len(chunks) == chunk_idx + 1:
+    #         # Check if adding current sentence's words would exceed max words limit
+    #         if len(chunks[chunk_idx]) + len(sentence.split(' ')) <= max_words_in_chunk:
+    #             chunks[chunk_idx].extend(sentence.split(' '))
+    #         else:
+    #             chunk_idx += 1
+    #             # Append sentence words into the current chunk
+    #             chunks.append(sentence.split(' '))
+    #     else:
+    #         print(chunk_idx)
+    #         # Append sentence words into the current chunk
+    #         chunks.append(sentence.split(' '))
 
-    # Join words back into sentences, this is that the TF pipeline expects
-    chunks = [' '.join(chunks[chunk_idx]) for chunk_idx in range(len(chunks))]
+    # # Join words back into sentences, this is that the TF pipeline expects
+    # chunks = [' '.join(chunks[chunk_idx]) for chunk_idx in range(len(chunks))]
 
-    chunk_summaries = hf_summarizer(chunks, max_length=240, min_length=120, do_sample=False)
+    # chunk_summaries = hf_summarizer(chunks, max_length=240, min_length=120, do_sample=False)
 
-    ## Merge chunk summaries and replace leading spaces
-    summary = ' '.join([summ['summary_text'] for summ in chunk_summaries]).replace(' .', '.').replace(' !', '!').replace(' ?', '?').replace("\\","")[1:]
+    # ## Merge chunk summaries and replace leading spaces
+    # summary = ' '.join([summ['summary_text'] for summ in chunk_summaries]).replace(' .', '.').replace(' !', '!').replace(' ?', '?').replace("\\","")[1:]
 
-    return summary
+    # return summary
 
 def summarize_tfidf(text,top_sents_num=4):
     sentences = detect_sentences(text,sent_detector=sent_detector)
