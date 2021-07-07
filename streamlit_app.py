6d6f4cf783f0bba03b436e83c74673c7942c1163
tomwalchak <tom@tomwalczak.com>
2021-07-07 23:58:49 +0100

    redirect to dedicated server

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.set_page_config(layout="wide", page_title="ML Portfolio")
+st.set_page_config(layout="wide", page_title="Abstract Analyzer", page_icon="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/72/twitter/282/beer-mug_1f37a.png", initial_sidebar_state="expanded",)
 
-url = 'https://github.com/tomwalczak'
+url = 'http://tomwalczak.com/pubmed-abstract-analyzer/'
 
-st.markdown('## My projects page: [LINK]({})'.format(url))
+st.markdown('## I have moved the project to: [LINK]({})'.format(url))
